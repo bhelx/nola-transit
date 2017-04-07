fb538dd281fc818cdfeb27ce78ed0d20257bb930
Benjamin Eckel <bhelx@simst.im>
2017-04-07 17:27:46 -0500

    use css circle markers

--- create_markers.rb
+++ create_markers.rb
@@ -7,10 +7,12 @@ colors = rows.map(&:first).uniq
 
 # Create 2 markers per color
 colors.each do |color|
+  puts "Writing markers with color: #{color}"
+
+  # Make small stop marker
   canvas = Magick::Image.new(40, 40) do |c|
     c.background_color = 'Transparent'
   end
-
   circle = Magick::Draw.new
   circle.stroke("##{color}")
   circle.fill_opacity(0)
@@ -18,14 +20,12 @@ colors.each do |color|
   circle.stroke_width(5)
   circle.ellipse(canvas.rows/2, canvas.columns/2, 15, 15, 0, 360)
   circle.draw(canvas)
-
-  puts "Writing marker with color: #{color}"
   canvas.write("./assets/markers/#{color}.gif")
 
+  # Make small transulcent stop marker
   canvas = Magick::Image.new(40, 40) do |c|
     c.background_color = 'Transparent'
   end
-
   circle = Magick::Draw.new
   circle.stroke("##{color}")
   circle.stroke_opacity(0.5)
@@ -34,9 +34,19 @@ colors.each do |color|
   circle.stroke_width(5)
   circle.ellipse(canvas.rows/2, canvas.columns/2, 15, 15, 0, 360)
   circle.draw(canvas)
-
-  puts "Writing translucent marker with color: #{color}"
   canvas.write("./assets/markers/#{color}_translucent.gif")
+
+  canvas = Magick::Image.new(60, 60) do |c|
+    c.background_color = 'Transparent'
+  end
+  circle = Magick::Draw.new
+  circle.stroke("#FFF")
+  circle.fill_opacity(1)
+  circle.fill("##{color}")
+  circle.stroke_width(7)
+  circle.ellipse(canvas.rows/2, canvas.columns/2, 23, 23, 0, 360)
+  circle.draw(canvas)
+  canvas.write("./assets/markers/#{color}_big.gif")
 end
 
 # Write the import file
@@ -46,6 +56,7 @@ colors.each do |color|
   js_file.write <<-JS
 import marker_#{color} from "./markers/#{color}.gif"
 import marker_#{color}_translucent from "./markers/#{color}_translucent.gif"
+import marker_#{color}_big from "./markers/#{color}_big.gif"
   JS
 end
 
@@ -57,6 +68,7 @@ colors.each do |color|
   js_file.write <<-JS
     "marker_##{color}": marker_#{color},
     "marker_##{color}_translucent": marker_#{color}_translucent,
+    "marker_##{color}_big": marker_#{color}_big,
   JS
 end
 
