700c1c469d987ec8894a3ef5bd908545e6cf154f
Marc Cenac <marc@codeforneworleans.org>
2017-04-21 17:28:25 -0500

    removing unused imports

--- android/app/src/main/java/com/transit/sqlite/SqliteModule.java
+++ android/app/src/main/java/com/transit/sqlite/SqliteModule.java
@@ -12,7 +12,6 @@ import com.facebook.react.bridge.ReactContextBaseJavaModule;
 import com.facebook.react.bridge.ReactMethod;
 import com.facebook.react.bridge.WritableArray;
 import com.facebook.react.bridge.WritableMap;
-import com.facebook.react.modules.core.DeviceEventManagerModule;
 import com.transit.R;
 import java.io.File;
 import java.io.FileNotFoundException;
