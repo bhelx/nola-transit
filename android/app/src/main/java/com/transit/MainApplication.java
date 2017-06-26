dd18285ff9d8c845bdcc1721f433d36f00f6ed04
Benjamin Eckel <bhelx@simst.im>
2017-06-26 18:34:14 -0500

    fix bad service id in database, perm issues as well

--- android/app/src/main/java/com/transit/MainApplication.java
+++ android/app/src/main/java/com/transit/MainApplication.java
@@ -5,6 +5,7 @@ import android.app.Application;
 import com.dieam.reactnativepushnotification.ReactNativePushNotificationPackage;
 import com.airbnb.android.react.maps.MapsPackage;
 import com.facebook.react.ReactApplication;
+import com.joshblour.reactnativepermissions.ReactNativePermissionsPackage;
 import com.smixx.fabric.FabricPackage;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
@@ -27,6 +28,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new ReactNativePermissionsPackage(),
             new FabricPackage(),
             new ReactNativePushNotificationPackage(),
           new VectorIconsPackage(),
